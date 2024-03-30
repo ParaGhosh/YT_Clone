40d74a01c4a2c37c49bb44280b655326e55fb1b6
Parashar Ghosh <parasharmusic333@gmail.com>
2024-03-30 12:50:37 +0530

    Skeleton Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Head from './components/Head';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Youtube</h1>
+    <div >
+      <Head />
+      <Body/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+/*
+Head
+Body
+  Sidebar
+    MenuItems
+  MainContainer
+    Button List
+    Video Conatiner
+      Video Card
+*/
